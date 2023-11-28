3a9fea9ff6a213b10d402c8bf63ff8a6da756b00
Md. Abdulla Al Mamun <abdulla.mamun@enosisbd.com>
2023-11-28 18:34:52 +0600

    Show only the front face of the cube.

--- src/components/CssOnlyAnimation/ImageCube.vue
+++ src/components/CssOnlyAnimation/ImageCube.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cuboid">
-    <div v-for="i in 6" class="cuboid__side" :key="i">
-      <img class="side-image" :src="`/images/flower-${Math.ceil(Math.random()*6)}.jpg`" :alt="`flower-${1}`">
+    <div class="cuboid__side" :key="i">
+      <img class="side-image" :src="`/images/flower-${Math.ceil(Math.random()*6)}.jpg`" :alt="`flower`">
     </div>
   </div>
 </template>
@@ -33,7 +33,7 @@
 .cuboid > div:nth-of-type(1) {
   transform: translate(-50%, -50%) rotateX(-90deg) translate3d(0, 0, calc((var(--width) / 2)));
 }
-
+/*
 .cuboid > div:nth-of-type(2) {
   transform: translate(-50%, -50%) rotateX(-90deg) rotateY(180deg) translate3d(0, 0, calc((var(--width) / 2)));
 }
@@ -53,7 +53,7 @@
 .cuboid > div:nth-of-type(6) {
   transform: translate(-50%, -50%) translate3d(0, 0, calc((var(--width) / 2) * -1)) rotateX(180deg);
 }
-
+*/
 .cuboid__side > .side-image {
   width: 100%;
   height: 100%;
@@ -61,10 +61,10 @@
 
 @keyframes cubeReverseAnimation {
   0% {
-    transform: rotateZ(0deg) translate3d(0, 0, 5vmin);
+    transform: rotateZ(0deg) translate3d(0, 0, calc(var(--width)/2));
   }
   100% {
-    transform: rotateZ(360deg) translate3d(0, 0, 5vmin);
+    transform: rotateZ(360deg) translate3d(0, 0, calc(var(--width)/2));
   }
 }
 </style>
