9500aefa81bba852570683a2f2fcccbce1e4c32d
Md. Abdulla Al Mamun <abdulla.mamun@enosisbd.com>
2023-12-01 17:23:57 +0600

    Refactor AppBar component to remove custom styling.

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,47 +1,49 @@
 <template>
-  <div class="pa-2 w-100 fill-height d-flex justify-space-between">
-    <div class="d-flex align-center">
-      <img class="d-none d-lg-inline-block fill-height" src="@/assets/logo.png" alt="logo"/>
-      <v-icon v-show="!openNavDrawer" class="d-lg-none" size="x-large" @click="toggleNavDrawer">
-        mdi-menu
-      </v-icon>
-      <v-icon v-show="openNavDrawer" class="d-lg-none" size="x-large" @click="toggleNavDrawer">
-        mdi-close
-      </v-icon>
-      <h1 class="ml-2">Header</h1>
-    </div>
-    <div class="d-flex align-center">
-      <v-menu>
-        <template v-slot:activator="{props}">
-          <v-btn v-bind="props" icon>
-            <v-avatar variant="outlined">
-              <v-img :src="store.getters.authenticatedUser?.profilePicture"/>
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item
-            @click="$router.push({path: '/profile'})"
-            :class="{'text-primary-lighten-1': $route.path !== '/profile',
-             'bg-primary-lighten-1 text-on-primary-lighten-1': $route.path === '/profile'}">
-            Profile
-          </v-list-item>
-          <v-list-item
-            append-icon="mdi-logout"
-            variant="text"
-            class="text-primary-lighten-1"
-            @click="logout">
-            Logout
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-  </div>
+  <v-avatar class="d-none d-lg-inline ml-2" rounded>
+    <v-img :src="logo" alt="logo"/>
+  </v-avatar>
+
+  <v-app-bar-nav-icon class="d-lg-none">
+    <v-icon v-show="!openNavDrawer" size="x-large" @click="toggleNavDrawer">
+      mdi-menu
+    </v-icon>
+    <v-icon v-show="openNavDrawer" size="x-large" @click="toggleNavDrawer">
+      mdi-close
+    </v-icon>
+  </v-app-bar-nav-icon>
+
+  <v-toolbar-title>Header</v-toolbar-title>
+
+  <v-menu>
+    <template v-slot:activator="{props}">
+      <v-btn icon>
+        <v-avatar v-bind="props" variant="outlined">
+          <v-img :src="store.getters.authenticatedUser?.profilePicture"/>
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item
+        @click="$router.push({path: '/profile'})"
+        :class="{'text-primary-lighten-1': $route.path !== '/profile',
+               'bg-primary-lighten-1 text-on-primary-lighten-1': $route.path === '/profile'}">
+        Profile
+      </v-list-item>
+      <v-list-item
+        append-icon="mdi-logout"
+        variant="text"
+        class="text-primary-lighten-1"
+        @click="logout">
+        Logout
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
 
 <script setup>
 import {useStore} from "vuex";
 import {useRouter} from "vue-router";
+import logo from '@/assets/logo.svg';
 
 const props = defineProps({
   openNavDrawer: {
