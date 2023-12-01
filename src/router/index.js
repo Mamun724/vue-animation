41c7d2fb1aa583aa23a08393af594fc629359487
Md. Abdulla Al Mamun <abdulla.mamun@enosisbd.com>
2023-12-01 11:54:31 +0600

    Create routes and update route security.

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,21 @@ import {createRouter, createWebHistory} from 'vue-router'
 import store from "@/plugins/store";
 import NotFound from "@/views/NotFound.vue";
 import Profile from "@/views/Profile.vue";
+import Assets from "@/views/Assets.vue";
+import Memories from "@/views/Memories.vue";
+import Settings from "@/views/Settings.vue";
 
 const routes = [
   {
     path: '/',
     component: () => import('@/layouts/AppLayout.vue'),
+    beforeEnter: (_from, _to) => {
+      if (store.getters.isAuthenticated) {
+        return true;
+      } else {
+        return {name: "login"};
+      }
+    },
     children: [
       {
         path: '',
@@ -15,19 +25,27 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (Home-[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/Home.vue'),
-        beforeEnter: (_from, _to) => {
-          if (store.getters.isAuthenticated) {
-            return true;
-          } else {
-            return {name: "login"};
-          }
-        }
+        component: () => import('@/views/Home.vue')
       },
       {
         path: 'profile',
         name: 'Profile',
         component: Profile
+      },
+      {
+        path: 'assets',
+        name: 'Assets',
+        component: Assets
+      },
+      {
+        path: 'memories',
+        name: 'Memories',
+        component: Memories
+      },
+      {
+        path: 'settings',
+        name: 'Settings',
+        component: Settings
       }
     ],
   },
